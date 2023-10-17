48554dadc138b6b0b67e4f6db9445a4c3ab81a4d
Esther Duncan <esther@magnusandmaeve.com>
2023-10-17 12:43:29 -0400

    Add files via upload

--- Quiz1_Master/js/slickQuiz-config.js
+++ Quiz1_Master/js/slickQuiz-config.js
@@ -15,58 +15,57 @@ var quizJSON = {
     },
     "questions": [
         { // Question 1 - Multiple Choice, Single True Answer
-            "q": "What number is the letter A in the English alphabet?",
+            "q": "What does the acronym KPI stand for?",
             "a": [
-                {"option": "8",      "correct": false},
-                {"option": "14",     "correct": false},
-                {"option": "1",      "correct": true},
-                {"option": "23",     "correct": false} // no comma here
+                {"option": "Key Predictive Indicator",      "correct": false},
+                {"option": "Key Predictor Index",     "correct": false},
+                {"option": "Key Performance Indicator",      "correct": true} // no comma here
             ],
-            "correct": "<p><span>That's right!</span> The letter A is the first letter in the alphabet!</p>",
-            "incorrect": "<p><span>Uhh no.</span> It's the first letter of the alphabet. Did you actually <em>go</em> to kindergarden?</p>" // no comma here
+            "correct": "<p><span>That's right!</span> correct answer text goes here</p>",
+            "incorrect": "<p><span>Sorry.</span> incorrect answer text goes here</p>" // no comma here
         },
-        { // Question 2 - Multiple Choice, Multiple True Answers, Select Any
-            "q": "Which of the following best represents your preferred breakfast?",
+        { // Question 2 - Multiple Choice, Single True Answer
+            "q": "What is a KPI?",
             "a": [
-                {"option": "Bacon and eggs",               "correct": false},
-                {"option": "Fruit, oatmeal, and yogurt",   "correct": true},
-                {"option": "Leftover pizza",               "correct": false},
-                {"option": "Eggs, fruit, toast, and milk", "correct": true} // no comma here
+                {"option": "A way to assess employee behaviour",      "correct": false},
+                {"option": "A synopsis of a business' digital marketing",     "correct": false},
+                {"option": "A measurable value for tracking business goals",      "correct": true},
+                {"option": "A look back at all the previous year's objectives",      "correct": false} // no comma here
             ],
-            "select_any": true,
-            "correct": "<p><span>Nice!</span> Your cholestoral level is probably doing alright.</p>",
-            "incorrect": "<p><span>Hmmm.</span> You might want to reconsider your options.</p>" // no comma here
-        },
-        { // Question 3 - Multiple Choice, Multiple True Answers, Select All
-            "q": "Where are you right now? Select ALL that apply.",
+            "correct": "<p><span>That's right!</span> correct answer text goes here</p>",
+            "incorrect": "<p><span>Sorry.</span> incorrect answer text goes here</p>" // no comma here
+        },        
+        { // Question 3 - Multiple Choice, Single True Answer
+            "q": "Which of the following refers to a KPI that focuses on past performance?",
             "a": [
-                {"option": "Planet Earth",           "correct": true},
-                {"option": "Pluto",                  "correct": false},
-                {"option": "At a computing device",  "correct": true},
-                {"option": "The Milky Way",          "correct": true} // no comma here
+                {"option": "Leading indicator",      "correct": false},
+                {"option": "Leaning indicator",     "correct": false},
+                {"option": "Lagging indicator",      "correct": true},
+                {"option": "None of the above",      "correct": false} // no comma here
             ],
-            "correct": "<p><span>Brilliant!</span> You're seriously a genius, (wo)man.</p>",
-            "incorrect": "<p><span>Not Quite.</span> You're actually on Planet Earth, in The Milky Way, At a computer. But nice try.</p>" // no comma here
-        },
-        { // Question 4
-            "q": "How many inches of rain does Michigan get on average per year?",
+            "correct": "<p><span>That's right!</span> correct answer text goes here</p>",
+            "incorrect": "<p><span>Sorry.</span> incorrect answer text goes here</p>" // no comma here
+        },        
+        { // Question 4 - Multiple Choice, Single True Answer
+            "q": "KPIs measure financial and non-financial metrics?",
             "a": [
-                {"option": "149",    "correct": false},
-                {"option": "32",     "correct": true},
-                {"option": "3",      "correct": false},
-                {"option": "1291",   "correct": false} // no comma here
+                {"option": "True",      "correct": true},
+                {"option": "False",     "correct": false} // no comma here
             ],
-            "correct": "<p><span>Holy bananas!</span> I didn't actually expect you to know that! Correct!</p>",
-            "incorrect": "<p><span>Fail.</span> Sorry. You lose. It actually rains approximately 32 inches a year in Michigan.</p>" // no comma here
-        },
-        { // Question 5
-            "q": "Is Earth bigger than a basketball?",
+            "correct": "<p><span>That's right!</span> correct answer text goes here</p>",
+            "incorrect": "<p><span>Sorry.</span> incorrect answer text goes here</p>" // no comma here
+        },                    
+        { // Question 5 - Multiple Choice, Single True Answer
+            "q": "Which of the following is not a KPI?",
             "a": [
-                {"option": "Yes",    "correct": true},
-                {"option": "No",     "correct": false} // no comma here
+                {"option": "Cash Flow",      "correct": false},
+                {"option": "Customer Complaints",     "correct": false},
+                {"option": "Projects on Budget",      "correct": false},
+                {"option": "Share Rate (Shares or ReTweets)",      "correct": false},
+                {"option": "They are all KPIs",      "correct": true} // no comma here
             ],
-            "correct": "<p><span>Good Job!</span> You must be very observant!</p>",
-            "incorrect": "<p><span>ERRRR!</span> What planet Earth are <em>you</em> living on?!?</p>" // no comma here
+            "correct": "<p><span>That's right!</span> correct answer text goes here</p>",
+            "incorrect": "<p><span>Sorry.</span> incorrect answer text goes here</p>" // no comma here
         } // no comma here
     ]
 };
